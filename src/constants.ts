7a55f8c1792c2e438cd8f5fd3fa1f2dcdb853e60
YKDByJK <Jasonk@glaa.net>
2024-06-12 21:10:46 -0400

    bruh

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? 'https://mainne
 
 // Solana address that will receive fees when somebody plays on this platform
 export const PLATFORM_CREATOR_ADDRESS = new PublicKey(
-  'HvX1mNoVH7EaqB6KDdgzsHageuNBvbgrpPvnNcEpcTva',
+  'AFnpa4BGHUwGKfrerZf7xDciC5EyeyEXKdnLCV6Kw7os',
 )
 
 // Gamba explorer URL - Appears in RecentPlays
